69455fa21093594b714e2d9bc07d1dc79abeb86b
Edgar A Leon <800736+eleon@users.noreply.github.com>
2021-10-08 16:58:51 -0700

    Adding a simple test for the Python interface.

---
+++ python/test-simple.py
@@ -0,0 +1,39 @@
+
+# PYTHONPATH=$PYTHONPATH:/Users/leonborja1/work/repos/mpibind/install/share python test-simple.py
+
+import os
+import mpibind
+
+# Todo: Update python/README
+#       Use spack load ... instead of pip install,
+#       but pip install could also be used. 
+
+# This simple example does not use MPI, thus 
+# specify my rank and total number of tasks 
+rank = 2
+ntasks = 4
+
+# Is sched_getaffinity supported? 
+getaffinity = True if hasattr(os, 'sched_getaffinity') else False 
+
+if getaffinity:
+    cpus = sorted(os.sched_getaffinity(0))
+    print('Running on ', len(cpus), 'cpus: ', cpus)
+
+# Create a handle
+# Num tasks is a required parameter
+handle = mpibind.MpibindHandle(ntasks)
+
+# Create the mapping 
+handle.mpibind()
+
+# Print the mapping
+handle.mapping_print()
+
+# Apply the mapping as if I am worker 'rank'
+# This function is not supported on some platforms
+if getaffinity:
+    handle.apply(rank)
+    cpus = sorted(os.sched_getaffinity(0))
+    print('Running on ', len(cpus), 'cpus: ', cpus)
+
