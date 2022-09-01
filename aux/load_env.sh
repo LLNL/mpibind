a1c77b6efe4a2c33b5669862d775c3620974d69e
Edgar A Leon <800736+eleon@users.noreply.github.com>
2022-08-31 17:52:22 -0700

    Restoring internal load_env.sh

--- aux/load_env.sh
+++ aux/load_env.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-#spack load pkgconf
-spack load m4/hwz6doo
-spack load autoconf/kcfww4q
-spack load automake/j4uvdos
-spack load libtool/fac54ff
+spack load pkgconf
+spack load m4
+spack load autoconf
+spack load automake
+spack load libtool
 
 spack load py-cffi
 
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$HOME/work/repos/libtap/install/lib/pkgconfig
-
-# pycotap is install under ~/.local/
-#export PYTHONPATH=$PYTHONPATH:$HOME/work/repos/pycotap/install/lib/python3.8/site-packages
