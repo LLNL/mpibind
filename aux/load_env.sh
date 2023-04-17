a1db1a74e103120f22df7fc6810555c4bf24bead
Edgar A Leon <800736+eleon@users.noreply.github.com>
2023-04-17 15:56:03 -0700

    Rename tioga topology file.

--- aux/load_env.sh
+++ aux/load_env.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-spack load pkgconf
-spack load m4
-spack load autoconf
-spack load automake
-spack load libtool
+#spack load pkgconf
+spack load m4/hwz6doo
+spack load autoconf/kcfww4q
+spack load automake/j4uvdos
+spack load libtool/fac54ff
 
 spack load py-cffi
 
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$HOME/work/repos/libtap/install/lib/pkgconfig
+
+# pycotap is install under ~/.local/
+#export PYTHONPATH=$PYTHONPATH:$HOME/work/repos/pycotap/install/lib/python3.8/site-packages
